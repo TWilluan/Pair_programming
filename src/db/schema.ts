53ed7b15fb5b4065281c086d5c6ec29ab8beee8b
Tuan Vo <tuanvo.will@gmail.com>
2024-04-30 04:31:55 -0400

    add drizzle-kit

---
+++ src/db/schema.ts
@@ -0,0 +1,13 @@
+import { integer, pgEnum, pgTable, serial, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
+// declaring enum in database
+
+export const popularityEnum = pgEnum('popularity', ['unknown', 'known', 'popular']);
+
+export const countries = pgTable('countries', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }),
+}, (countries) => {
+  return {
+    nameIndex: uniqueIndex('name_idx').on(countries.name),
+  }
+});
