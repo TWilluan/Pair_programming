9e520ebe670cb0e9da85b39b8e93e4b3cd3e6b09
Tuan Vo <tuanvo.will@gmail.com>
2024-05-01 05:07:02 -0400

    fix module bug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import GoogleProvider from "next-auth/providers/google"
 import DiscordProvider from "next-auth/providers/discord"
-import { db } from "@/src/db"
+import { db } from "@/db"
 import type { Adapter } from "next-auth/adapters"
 
 const handler = NextAuth({
---
+++ src/app/create-room/page.tsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+import { CreateRoomForm } from "./create-form";
+
+const CreateRoom = () => {
+    return(<>
+        <div>
+            hello world
+        </div>
+        <CreateRoomForm/>
+    </>)
+}
+
+
+export default CreateRoom;
